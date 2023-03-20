878ccd746d72013a3e07a756bd156adaec508d96
Abdur Rahman <ar7862204@gmail.com>
2023-03-20 19:25:43 +0530

    20 march

--- PW.java
+++ PW.java
@@ -1,8 +1,18 @@
-class PW{
-public void m1(){
-////code
+class PW {
+public static void main(String args[]){
+int n = 11;
+for(int i = 0; i<n; i++){
+    for(int j = 0; j<n; j++){
+if(i==j || (i+j)==(n-1) ||i==0 || j==0 || i==n-1 || j==n-1||i+j==(n-1)/2){
+System.out.print("*");
 }
-public void m2(){
-    ////codeisisisis
+else{
+    System.out.print(" ");
+}
+
+    }
+    System.out.println();
+}
+
 }
 }
